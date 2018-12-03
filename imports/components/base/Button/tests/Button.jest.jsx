2de78a1224cc7b4cf322cb113abe0405c2b5b1fe
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2018-12-02 19:05:18 -0500

    adding react-testing-library tests for Button

--- imports/components/base/Button/tests/Button.jest.jsx
+++ imports/components/base/Button/tests/Button.jest.jsx
@@ -3,12 +3,37 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
 import renderer from "react-test-renderer";
+import { cleanup, render } from "react-testing-library";
 import "jest-styled-components";
+import "jest-dom/extend-expect";
 import Button from "..";
+
+afterEach(cleanup);
 
 describe("<Button />", () => {
   it("should match render snapshot", () => {
     const tree = renderer.create(<Button>Test</Button>).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  describe("Practice: testing variations", () => {
+    it("react-test-renderer: should have class color palevioletred", () => {
+      const tree = renderer.create(<Button>Test</Button>).toJSON();
+      expect(tree).toHaveStyleRule("color", "palevioletred");
+    });
+
+    it("react-testing-library: should have class color palevioletred", () => {
+      const { container } = render(<Button>Test</Button>);
+      expect(container.firstChild).toHaveStyleRule("color", "palevioletred");
+    });
+
+    it("react-testing-library, jest-dom: should have disabled property when disabled", () => {
+      const { container } = render(
+        <Button disabled data-testid="test-button">
+          Test
+        </Button>,
+      );
+      expect(container.firstChild).toHaveAttribute("disabled");
+    });
   });
 });
